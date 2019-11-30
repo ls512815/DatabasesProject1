7bf629e89bd7fcbe2bda5cdf49035a70ec88969b
Lydia Shiffler <ls512815@ohio.edu>
2019-11-30 11:42:12 -0500

    q

--- prog1-1.java
+++ prog1-1.java
@@ -160,7 +160,7 @@ class members {
                         rset3.getString(1) + ", and experience all the new wonderful pieces");
   } 
   ResultSet rset4 = stmt.executeQuery(query4);
-  System.out.println("      Special thanks to our donors, ");
+  System.out.println("\n" + "      Special thanks to our donors, ");
   while (rset4.next ()) { 
     System.out.println(rset4.getString(1) + ", ");
   } 
