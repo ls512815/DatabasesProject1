176b1a15ec3325de9198774b2ac7e9a61c3474f0
Lydia Shiffler <ls512815@ohio.edu>
2019-11-30 10:59:56 -0500

    q

--- prog1-1.java
+++ prog1-1.java
@@ -135,8 +135,8 @@ class members {
   while (rset.next ()) { 
     System.out.println("           There are some new paintings arriving on " +
                         rset.getString(1).substring(0,10) + "! " + "\n" +
-                        "           ____________________________________________________" + "\n" + "\n" +
-                        "There will be paintings from some of our best artists, including: ");
+                        "        ________________________________________________________" + "\n" + "\n" +
+                        "           There will be paintings from some of our best artists, including: ");
   } 
   ResultSet rset2 = stmt.executeQuery(query2);
   System.out.println("");
