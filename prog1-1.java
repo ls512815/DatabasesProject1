150cbdeeaae97d703887075a5f6673a0c2436709
Lydia Shiffler <ls512815@ohio.edu>
2019-12-02 16:22:38 -0500

    f

--- prog1-1.java
+++ prog1-1.java
@@ -166,9 +166,9 @@ class members {
   } 
 
   ResultSet rset4 = stmt.executeQuery(query4);
-  System.out.println("We are featuring the following pieces of art: ");
+  System.out.println("      We are featuring the following pieces of art: ");
   while (rset4.next ()) { 
-    System.out.print(rset4.getString(1) + " ");
+    System.out.print("        " + rset4.getString(1) + " ");
   } 
 
   ResultSet rset5 = stmt.executeQuery(query5);
